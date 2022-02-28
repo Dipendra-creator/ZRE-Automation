6dd7fcc8a07fc2423e53b90915a5e66fdf98e60b
dipendra-react <66115431+Dipendra-creator@users.noreply.github.com>
2022-02-28 18:45:30 +0530

    Finalize

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 # Date and time
 from datetime import date
 from datetime import timedelta
-import json_extract, text_parser
+import json_extract, text_parser, os
 
 # Get the current date
 today = date.today()
 print("Today is:", today)
+
+# If Class JSON folder does not exist, create it
+if not os.path.exists('Class JSON'):
+    os.makedirs('Class JSON')
+
+# If Class Recording folder does not exist, create it
+if not os.path.exists('Class Recording'):
+    os.makedirs('Class Recording')
 
 """
     Yesterday's date = 1
